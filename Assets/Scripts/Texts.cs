1863282df1440af92aa76a52dd180d99bc1cbcfd
Cuagga <66082268+Cuagga@users.noreply.github.com>
2022-02-10 04:39:31 +0100

    Add files via upload

--- Assets/Scripts/Texts.cs
+++ Assets/Scripts/Texts.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Texts", order = 1)]
 public class Texts : ScriptableObject
 {
-    [SerializeField] private string text = default;
-    public string Text => text;
+    [SerializeField] private string[] text;
+
+    public string getText(int k) {
+        return text[k];
+    }
 
 }
